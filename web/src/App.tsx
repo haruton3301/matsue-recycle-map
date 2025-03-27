b5545e688d9a00d5db4e9db72af08fce788acff5
Haruto Ikehara <kduxwr@gmail.com>
2025-03-28 08:58:30 +0900

    Add analytics

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,16 @@
+import { logEvent } from "firebase/analytics"
+import { useEffect } from "react"
 import { Footer } from "./components/Footer"
 import { Header } from "./components/Header"
 import { MapView } from "./components/MapView"
 import { Message } from "./components/Message"
+import { analytics } from "./lib/firebase"
 
 function App() {
+  useEffect(() => {
+    logEvent(analytics, "page_view")
+  }, [])
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
