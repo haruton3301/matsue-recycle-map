2eb83ecdb04f2cbf632c62a0bbe778f87fb2fca1
Haruto Ikehara <kduxwr@gmail.com>
2025-03-28 09:00:29 +0900

    Fix text size

--- web/src/components/Message.tsx
+++ web/src/components/Message.tsx
@@ -1,5 +1,5 @@
 export const Message = () => (
-  <div className="mb-5 md:text-base text-sm">
+  <div className="mb-5">
     松江市のリサイクルステーションの位置にピンを表示しています。検索欄に町名を入力して「移動」を押すことで、入力した町に移動します。
   </div>
 )
